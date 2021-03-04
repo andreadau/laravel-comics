f4c5d474b56a791e91334e8d8f0efebecd9c4218
Andrea <dauriaandrea99@gmail.com>
2021-03-04 13:48:58 +0100

    Must Reads Style ADD

--- resources/views/guests/comics/index.blade.php
+++ resources/views/guests/comics/index.blade.php
@@ -6,7 +6,25 @@
             <h1>Comics & Graphic novels</h1>
         </div>
     </div>
+    <!-- comics container -->
     <div class="comics_container">
+        <ul class="nav justify-content-center text-uppercase ">
+            <li class="nav-item">
+                <a class="nav-link" href="#">This week</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Last week</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Next week</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Upcoming</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">See all</a>
+            </li>
+        </ul>
         <div class="comics">
             <img src="img/batman.jpg" alt="">
             <h4 class="title">BATMAN</h4>
@@ -32,5 +50,35 @@
             <h4 class="title">BATMAN</h4>
             <p class="availability">AVAIABLE NOW</p>
         </div>
-    </div> 
+    </div>
+    <!-- must reads -->
+    <div class="container py-5 must_reads_container">
+            <h1 class="text-uppercase">must reads</h1>
+        <div class="must_reads">
+            <div class="must_reads_comics">
+                <img src="img/connect.jpg" alt="">
+                <h4 class="subtitle text-uppercase">Lorem ipsum dolor sit amet consectetur</h4>
+                <h3 class="title text-uppercase">Lorem ipsum dolor</h3>
+                <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Natus sapiente voluptatem ipsam vel, accusantium fugit.</p>
+            </div>
+            <div class="must_reads_comics">
+                <img src="img/sandman.jpg" alt="">
+                <h4 class="subtitle text-uppercase">Lorem ipsum dolor sit amet consectetur</h4>
+                <h3 class="title text-uppercase">Lorem ipsum dolor</h3>
+                <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Natus sapiente voluptatem ipsam vel, accusantium fugit.</p>
+            </div>
+            <div class="must_reads_comics">
+                <img src="img/wonder.jpg" alt="">
+                <h4 class="subtitle text-uppercase">Lorem ipsum dolor sit amet consectetur</h4>
+                <h3 class="title text-uppercase">Lorem ipsum dolor</h3>
+                <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Natus sapiente voluptatem ipsam vel, accusantium fugit.</p>
+            </div>
+        </div>
+    </div>
+    <!-- current series -->
+    <div class="current_series">
+    </div>
 @endsection
